14ee1c4636f77e7b18c152b9c28d035bf88154be
Abhinav Deokuliar <113229623+AbhinavDeokuliar@users.noreply.github.com>
2024-03-06 23:01:39 +0530

    another check

--- draft2.py
+++ draft2.py
@@ -1,50 +1,54 @@
 import webbrowser
-
 import folium
 from folium.features import CustomIcon
-from jinja2 import Template
 
 
 def auto_open(path, f_map):
+    """This function opens the file in browser automatically"""
     html_page = f'{path}'
     f_map.save(html_page)
-    # open in browser.
     new = 2
     webbrowser.open(html_page, new=new)
 
 
-popup_template = Template("""
-
-""")
+"""Starting point of the map"""
 
 latitude = 30.7688
 longitude = 76.5754
 
+"""Various tile layers used in the map"""
+
 tile_layer1 = folium.TileLayer(tiles="Cartodb dark_matter")
+tile_layer2 = folium.TileLayer(tiles="OpenStreetMap.DE",
+                               attr='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> '
+                                    'contributors')
+tile_layer3 = folium.TileLayer(tiles="OpenStreetMap.France",
+                               attr='&copy; OpenStreetMap France | &copy; <a '
+                                    'href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors')
 tile_layer4 = folium.TileLayer(tiles="OpenStreetMap.BZH",
                                attr='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> '
                                     'contributors, Tiles courtesy of <a href="http://www.openstreetmap.bzh/" '
                                     'target="_blank">Breton OpenStreetMap Team</a>')
-tile_layer3 = folium.TileLayer(tiles="OpenStreetMap.France",
-                               attr='&copy; OpenStreetMap France | &copy; <a '
-                                    'href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors')
-tile_layer2 = folium.TileLayer(tiles="OpenStreetMap.DE",
-                               attr='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> '
-                                    'contributors')
+
+"""List of point of interests"""
 
 block = [[30.76714843670572, 76.57472148622176], [30.766843747902985, 76.57621805141478]]  # [ c3, c1]
 food = [[30.768744947318915, 76.5776381704867], [30.768756919536795, 76.5777927058071]]  # [creative food, crunchy bite]
+
+"""Icon Urls & its implementation """
+
 food_icon_url = "https://pics.freeicons.io/uploads/icons/png/10274305501591055829-512.png"
 book_icon_url = "https://pics.freeicons.io/uploads/icons/png/7141564811595156044-512.png"
 
 food_icon = CustomIcon(food_icon_url, icon_size=(25, 25))
 book_icon = CustomIcon(book_icon_url, icon_size=(25, 25))
 
-map_cu = folium.Map(location=[latitude, longitude], zoom_control=True, zoom_start=17, max_zoom=25, min_zoom=16,
-                    marker_zoom=17, tiles=tile_layer2)
+"""Map initialization"""
 
-tile_layer4.add_to(map_cu)
-tile_layer3.add_to(map_cu)
+map_cu = folium.Map(location=[latitude, longitude], zoom_control=False, scroll_wheel_zoom=False, zoom_start=17,
+                    marker_zoom=17, tiles=tile_layer4)
+
+"""Adding tiles layer in map"""
 
 food_courts = folium.FeatureGroup(name="Food_courts").add_to(map_cu)
 blocks = folium.FeatureGroup(name="Blocks").add_to(map_cu)
@@ -64,5 +68,6 @@ folium.Marker([30.76714843670572, 76.57472148622176], popup="C1", icon=book_icon
 folium.Marker([30.768744947318915, 76.5776381704867], popup="Creative Foods", icon=food_icon).add_to(food_courts)
 
 folium.LayerControl(collapsed=True).add_to(map_cu)
-map_cu.save("draft1.html")
-auto_open("draft1.html", map_cu)
+
+map_cu.save("draft2.html")
+auto_open("draft2.html", map_cu)
